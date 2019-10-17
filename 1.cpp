99e248637fe56cb860523de4d80c2efcf8185c83
Manubhav <54354723+Manubhav@users.noreply.github.com>
2019-10-17 14:06:37 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+  long long int input();
+ void output(long long int);    //RANGE OF LONG LONG INT IS +-9,223,372,036,854,775,807
+ int main()
+ {
+ long long int c;
+ c= input();
+ output(c);
+ }
+ long long int input()
+ {	
+ long long int b;
+ printf("Enter PRN:");
+ scanf("%lld",&b);
+ return b;
+ }
+ void
+ output(long long int a)	
+ {
+  printf("PRN is: %lld",a);
+ }	
